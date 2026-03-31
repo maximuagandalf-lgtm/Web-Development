ab677b58083029168394b56ea7908cbd5ce1a00e
Ayush Chauhan <agueroayushchauhan@gmail.com>
2026-03-31 20:20:58 +0530

    Get and use request in express js, static folders and URL setup of a website

--- Video 88/main.js
+++ Video 88/main.js
@@ -1,14 +1,37 @@
-const { createServer } = require('node:http');
+// 
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end("<h1>This is helloworld<h1>");
-});
+//app.get or app.put or app.delete or app.post('path', handler function)
+// app.get('/', (req, res) => {
+//   res.send('Hello World2!')
+// })
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// app.get('/about', (req, res) => {
+//   res.send('Hello about!')
+// })
+
+app.use(express.static('public')) //files inside public forlder will be available for puvlic to access, used to protect backend code
+
+app.get('/blog/:slug', (req, res) => {
+    //For URL: http://localhost:3000/blog/intro-to-padosi?mode=dark&region=in
+    console.log(req.params)//outputs the elemtns before ? in URL
+    console.log(req.query)//ouputs the elements after ? in URL
+  res.send(`hello ${req.params.slug}`)
+})
+// app.get('/blog', (req, res) => {
+//   res.send('Hello blog!')
+// })
+
+// app.get('/blog/intro-to-js', (req, res) => {
+//   res.send('Hello Javascript!')
+// })
+// app.get('/blog/intro-to-python', (req, res) => {
+//   res.send('Hello Python!')
+// })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
