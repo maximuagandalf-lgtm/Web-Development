a731c20cd05879098ecba935730860b2cfd1784b
Ayush Chauhan <agueroayushchauhan@gmail.com>
2026-04-09 11:25:12 +0530

    This is a file organizer, which moves the files into desired folders based on their extensions.

---
+++ Video 91/js/server.js
@@ -0,0 +1,22 @@
+import fs from "fs/promises"
+import fsn from "fs"
+import path from "path"
+
+let files = await fs.readdir("//Users//ayush//Desktop//Code files//Web Dev//Web Development//Video 91")
+
+const basepath = "//Users//ayush//Desktop//Code files//Web Dev//Web Development//Video 91"
+for (const item of files) {
+    let ext = item.split(".")[item.split(".").length-1] //splitting the words before "." starting from -1
+    console.log(ext)
+    if (fsn.existsSync(path.join(basepath, ext) && ext!= 'js' && ext!= 'json')){ //check wether the file xists or not
+        // moving the files to this directory if its not a js or json file
+        fs.rename(path.join(basepath, item), path.join(basepath, ext, item))  //fs.rename(oldpath, newpath) is used to move files from one dir to another dir
+    }
+    else{
+        fs.mkdir(ext)
+        fs.rename(path.join(basepath, item), path.join(basepath, ext, item))
+    }
+    
+    console.log(item)
+}
+
